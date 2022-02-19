3cd9ef9a895cf7881a3f7f3f41f2b9f7424a15f8
Michael Shtarev <shtarev_me@teleformis.ru>
2022-02-19 18:50:28 +0300

    Initial commit.

--- src/main/java/com/example/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/example/taskmanager/TaskManagerApplication.java
@@ -8,5 +8,6 @@ public class TaskManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
+        System.out.println("hello");
     }
 }
